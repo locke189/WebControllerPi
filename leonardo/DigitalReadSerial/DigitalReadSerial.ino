24f96517d51d17cce4b32f90ed3ea80da61ff469
Juan David Insuasti Bravo <juan.insuasti@gmail.com>
2015-12-03 21:13:09 -0500

    Arduino sketch rewritten.. Still sequential code no interrupts

--- leonardo/DigitalReadSerial/DigitalReadSerial.ino
+++ leonardo/DigitalReadSerial/DigitalReadSerial.ino
@@ -1,105 +1,260 @@
-/* Simple Serial ECHO script : Written by ScottC 03/07/2012 */
+/* Sensor and actuator controller 2015/11 by Juan Insuasti*/
+/* FIRMWARE = V0.1
 
-#include "DHT.h" //cargamos la librería DHT
-#define DHTPIN 11 //Seleccionamos el pin en el que se //conectará el sensor
-#define DHTTYPE DHT11 //Se selecciona el DHT11 (hay //otros DHT)
-DHT dht(DHTPIN, DHTTYPE); //Se inicia una variable que será usada por Arduino para comunicarse con el sensor
+/* HARDWARE
+ *    -ARDUINO LEONARDO
+ */
+
+/* 
+ *  Contents
+ *  
+ *  $001$ Includes and defines
+ *  $002$ Global Variable Definitions
+ *  $003$ Setup and Initialization
+ *  $005$ Function Definitions
+ *  $006$ Main loop
+ *     
+ */
+
+
+/* $001$ Includes and defines */
+
+//Temperature and Humidity Sensor definitions DHT11
+#include "DHT.h"            //Loading of  DHT
+#define   DHTPIN     11     //DHT Sensor PIN
+#define   DHTTYPE    DHT11  //Sensor type selection DHT11 (There are other DHT sensors)
+DHT dht(DHTPIN, DHTTYPE);   //declaration of DHT object
+
+//Serial Port Definitions
+#define   SOH     0x01
+#define   EOT     0x04
+#define   RS      0x1E
+
+ 
+//Device definitions
+#define   LASER_ON      0x2B
+#define   LASER_OFF     0x2D
+#define   DEVICE_ID     0x41
+
+//Sensor definitions
+#define   SUMMARY         0x2F
+#define   LASER_TOGGLE    0x30
+#define   LASER_STATUS    0x31
+#define   LASER_ID        0x30
+#define   TEMPERATURE_ID  0x32
+#define   TEMP_OFFSET     7     // Offset debido al error de medicion
+#define   HUMIDITY_ID     0x33
+#define   DHT_POLL        5     // Poll sensor data each 5 interrupts
+#define   SENSOR_TIME     10000 // 10 seconds
+//Sensor/actuator pins
+#define LASER_PIN     12
+#define BOARD_LED     13
+
+
+//Serial parameters
+#define BITRATE       57600
+
+//Timer settings
+#define TIMEOUT       5000 // 5 seg
+
+
+/*   $002$ Global Variable Definitions */
 
 /* Use a variable called byteRead to temporarily store
    the data coming from the computer */
+
+// Serial Data
 int byteRead;
-boolean laser;
-boolean thisDevice;
-int thisDeviceNumber;
-float humidity;
-float temperature;
+String serialData;
+
+//Actuator Flags
+boolean   laser_state = false;
+
+
+//Sensor Variables and flags
+float     humidity = 0;
+float     temperature = 0;
+
+//Device Info and flags
+boolean  volatile deviceEnabled = false;
+boolean volatile  timeout = false;
+String    dataStream;
+int     volatile interruptCounter = 0;
+
+//Time
+unsigned long volatile timeStamp = 0;
+unsigned long volatile sensor_timeStamp = 0;
+
+
+/* $003$ Setup and Initialization  */
+
+
+//------Setup Functions---------
 
 void setup() {                
 // Turn the Serial Protocol ON
-  Serial1.begin(115200);
-  Serial.begin(115200);
-  pinMode (12, OUTPUT);
-  digitalWrite (12, LOW);
-  pinMode (13, OUTPUT);
-  digitalWrite (13, LOW);
-  
+  Serial1.begin(BITRATE);
+  Serial.begin(BITRATE);
+
+//Laser pin 12
+  pinMode(LASER_PIN, OUTPUT);
+  digitalWrite(LASER_PIN, LOW);
+
+//Enable signal to internal LED pin 13
+  pinMode(BOARD_LED, OUTPUT);
+  digitalWrite(BOARD_LED, LOW);
+
+//DHT sensor initialization
   dht.begin(); //Se inicia el sensor
-  
-  laser = false;
-  thisDeviceNumber = 0x41; //device "A"
+
+
+  sensor_timeStamp = millis();
 }
+
+//------------------------------------------
+
+
+
+/*    $005$ Function Definitions */
+
+void activateDevice(void){
+  deviceEnabled  = true;
+  digitalWrite(BOARD_LED, HIGH);
+  }
+
+
+void deactivateDevice(void){
+  deviceEnabled = false;
+  digitalWrite(BOARD_LED, LOW);
+  }  
+
+
+
+//------------------------------------------
+
+
+/*     $006$ Main loop   */
 
 void loop() {
- 
-
-   /*  check if data has been sent from the computer: */
+  /*  check if data has been sent from the master device:  */
   if (Serial1.available()) {
+    
     /* read the most recent byte */
+    //byteRead = Serial1.read();
+    serialData = Serial1.readStringUntil(EOT);
 
-   byteRead = Serial1.read();
-    /*ECHO the value that was read, back to the serial port. */
 
-   //handshake
-   if ((byteRead == thisDeviceNumber) && (thisDevice == false)){
-    thisDevice = true;
-    digitalWrite (13, HIGH);
-    Serial1.write(thisDeviceNumber);
-    Serial.write(thisDeviceNumber);
-    }
+   /* Serial handshake with Master Device */
+    if ((serialData[0] == SOH ) && (serialData[1] == DEVICE_ID) && (deviceEnabled == false)){
+        activateDevice();
+        Serial1.write(DEVICE_ID);
+        timeStamp = millis();
+     }
 
-   //operation "0" laser toggle hex30 
-   if( (byteRead == 0x30) && (thisDevice == true) && (laser == false) ){
+
+   /* Operation "/".  Summary of all sensor data and actuator status */
+    if( ( serialData[2] == SUMMARY ) && ( deviceEnabled == true )  ){
+
+          
+          //laser state
+          Serial1.write(RS);
+          Serial1.write(LASER_ID);
+          if( laser_state == true ){
+            Serial1.write(LASER_ON);
+            }
+          else{
+            Serial1.write(LASER_OFF);
+            }
     
-         digitalWrite (12, HIGH);
-         Serial1.write(0x30);
-         laser = true;
-         thisDevice = false;
-         digitalWrite (13, LOW);
-    }
+          //Temperature
+          Serial1.write(RS);
+          Serial1.write(TEMPERATURE_ID);
+          Serial1.print(temperature);
     
-    if( (byteRead == 0x30) && (thisDevice == true) && (laser == true) ){
+          //Humidity
+          Serial1.write(RS);
+          Serial1.write(HUMIDITY_ID);
+          Serial1.print(humidity);
     
-         digitalWrite (12, LOW);
-         Serial1.write(0x31);
-         laser = false;
-         thisDevice = false;
-         digitalWrite (13, LOW);
-    }
-
-    //operation "1" hex31 read laser status 
-    if( (byteRead == 0x31) && (thisDevice == true) ){   
-         Serial1.println(laser);
-         thisDevice = false;
-         digitalWrite (13, LOW);
-    }
-
-
-    //operation 2 => hex 32 read temperature value
-    if( (byteRead == 0x32) && (thisDevice == true) ){
-         //read temperature/humidity data
-         temperature = dht.readTemperature() - 7; //Se lee la temperatura - offset
-         Serial.println(temperature);
-         Serial1.println(temperature);
-         thisDevice = false;
-         digitalWrite (13, LOW);
-    }
-
-     //operation 3 => hex 33 read humidity value
-    if( (byteRead == 0x33) && (thisDevice == true) ){
-         humidity = dht.readHumidity(); //Se lee la humedad
-         Serial.println(humidity);
-         Serial1.println(humidity);
-         thisDevice = false;
-         digitalWrite (13, LOW);
-    }
-
-    //no actions available 
-    if(byteRead != 0x74){
-      //Serial1.write(0x40);
-      //thisDevice = false;
+          //end
+          Serial1.write(EOT);
       }
   
-  
+   
+   /*  Operation "0" laser toggle  */ 
+    if( (serialData[2] == LASER_TOGGLE) && (deviceEnabled == true) && (laser_state == false) ){
+         Serial1.write(LASER_ID);
+         Serial1.write(LASER_ON);
+         Serial1.write(0x04);
+         digitalWrite (LASER_PIN, HIGH);
+         laser_state = true;
+
+    }
+    
+    if( (serialData[2] == LASER_TOGGLE) && (deviceEnabled == true) && (laser_state == true) ){
+         Serial1.write(LASER_ID);
+         Serial1.write(LASER_OFF);
+         Serial1.write(EOT); 
+         digitalWrite (LASER_PIN, LOW);
+         laser_state = false;
+ 
+    }
+
+    /* Operation "1" hex31 read laser status */
+    if( (serialData[2] == LASER_STATUS) && (deviceEnabled == true) ){   
+         Serial1.write(LASER_ID);
+         if( laser_state == true ){
+            Serial1.write(byte(LASER_ON)); 
+          }
+          else{
+            Serial1.write(byte(LASER_OFF));
+            }
+          
+          //Serial1.write(byte(laser_state));
+
+    }
+
+
+    /* Operation 2 => hex 32 read temperature value */
+    if( (serialData[2] == TEMPERATURE_ID) && (deviceEnabled == true) ){
+         //read temperature/humidity data
+         //temperature       =   dht.readTemperature() - TEMP_OFFSET; //Se lee la temperatura - offset
+         Serial1.write(TEMPERATURE_ID);
+         Serial1.print(temperature);
+         Serial1.write(EOT);
+
+    }
+
+     /* Operation 3 => hex 33 read humidity value   */
+    if( (serialData[2] == HUMIDITY_ID) && (deviceEnabled == true) ){
+         //humidity = dht.readHumidity();
+         Serial1.write(HUMIDITY_ID);
+         Serial1.print(humidity);
+         Serial1.write(EOT);
+    }
+
+
+     //deactivate device at the end of serial read
+     deactivateDevice();
+
   }
+
+
+  //Sensor Updates! in main loop
+  if ( ((millis() - sensor_timeStamp) >= SENSOR_TIME) || ( ((millis() - sensor_timeStamp) < 0 ) ) ){
+    temperature       =   dht.readTemperature();
+    humidity = dht.readHumidity(); 
+    sensor_timeStamp = millis();
+    }
+
+
+       
 }
+
+//--------------------EOF-------------------
+
+
+
+
+
 
